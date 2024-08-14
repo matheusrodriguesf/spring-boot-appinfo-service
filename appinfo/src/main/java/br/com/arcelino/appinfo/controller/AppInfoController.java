365ab669fbc9446c50600e1f5fff130b6d24f50d
Matheus Rodrigues <matheus.arcelino@cidadania.gov.br>
2024-08-14 14:37:01 -0300

    Importacao Projeto Base

---
+++ appinfo/src/main/java/br/com/arcelino/appinfo/controller/AppInfoController.java
@@ -0,0 +1,27 @@
+package br.com.arcelino.appinfo.controller;
+
+import org.springframework.boot.info.BuildProperties;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.arcelino.appinfo.model.AppInfo;
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequestMapping("app")
+@RequiredArgsConstructor
+public class AppInfoController {
+
+    private final BuildProperties buildProperties;
+
+    @GetMapping("/info")
+    public ResponseEntity<AppInfo> getAppInfo() {
+        return ResponseEntity.ok(new AppInfo(
+                buildProperties.getName(),
+                buildProperties.getVersion(),
+                System.getProperty("java.version")));
+    }
+
+}
---
+++ appinfo/src/main/java/br/com/arcelino/appinfo/model/AppInfo.java
@@ -0,0 +1,7 @@
+package br.com.arcelino.appinfo.model;
+
+public record AppInfo(
+        String name,
+        String version,
+        String javaVersion) {
+}
